a76fdc48b204c2c35e76c4c4f4f0dd68c8718b13
morghull <morghull.study@gmail.com>
2021-10-14 01:28:28 +0300

    add more task and test script to task.js
    for testing validate method

--- js/tasks.js
+++ js/tasks.js
@@ -22,3 +22,15 @@ try {
 const validRValidator = new RangeValidator(12, 25);
 console.log('%c%s', validStyle, 'RangeValidator', validRValidator);
 console.log('%c%s', validStyle, 'Range', validRValidator.getRange);
+try {
+  const testNumber1 = validRValidator.validate(5);
+  console.log('%c%s', validStyle, 'testNumber1', testNumber1);
+} catch (error) {
+  console.log('%c%s', errorStyle, error);
+}
+try {
+   const testNumber2 = validRValidator.validate(5);
+   console.log('%c%s', validStyle, 'testNumber2', testNumber2);
+ } catch (error) {
+   console.log('%c%s', errorStyle, error);
+ }
